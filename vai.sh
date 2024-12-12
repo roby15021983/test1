3864af088a7bfc05fc6658e9fe41d1eb41c7f963
roby15021983 <74907792+roby15021983@users.noreply.github.com>
2024-12-12 13:28:02 +0100

    Update vai.sh

--- vai.sh
+++ vai.sh
@@ -303,11 +303,11 @@ msg_info "Starting LXC Container"
 pct start $CTID
 msg_ok "Started LXC Container"
 
-#lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/roby15021983/test1/refs/heads/main/vai1.sh)" || exit
-#IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
-#pct set $CTID -description "# ${APP} LXC
+lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/roby15021983/test1/refs/heads/main/vai1.sh)" || exit
+IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
+pct set $CTID -description "# ${APP} LXC
 #### https://tteck.github.io/Proxmox/
-#<a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/☕-Buy me a coffee-red' /></a>"
-#msg_ok "Completed Successfully!\n"
-#echo -e "${APP} should be started on TTY7
-           #  \n"
+<a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/☕-Buy me a coffee-red' /></a>"
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be started on TTY7
+             \n"
